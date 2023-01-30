e51be3229e5744b8c3e37874ae237ac30245f300
Amr Mohemd <93833053+Amrmohamed090@users.noreply.github.com>
2023-01-30 20:18:13 +0200

    mouse movement done

--- client/realtimeplotting.js
+++ client/realtimeplotting.js
@@ -3,8 +3,8 @@ live_input_container = document.getElementById("live-input")
 live_output_container = document.getElementById("live-output")
 var input_arr_y= new Array(200).fill(0);
 var input_arr_x= Array.from(Array(200).keys())
-console.log(input_arr_y)
-console.log(input_arr_x)
+var output_arr_y=new Array(200).fill(0);
+
 //=> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
@@ -31,20 +31,44 @@ function plotlyMultiLinePlot(container, data){
 }
 
 var canvas = document.getElementById("mousepad");
-canvas.addEventListener("mousemove", function (evt) {
+canvas.addEventListener("mousemove", async function (evt) {
     var mousePos = getMousePos(canvas, evt);
-    input_arr_y.push(mousePos.y)
-    input_arr_x.push(input_arr_x[input_arr_x.length-1]+1)
-    if(input_arr_y.length > 300){
-        input_arr_y.shift()
-        input_arr_x.shift()
-    }
-    console.log(input_arr_y.length + ',' + input_arr_x.length)
-    plotlyMultiLinePlot(live_input_container, [{x: input_arr_x, y: input_arr_y}])
+    input_arr_y.push(-mousePos.y+canvas.height)
 
+    if(input_arr_y.length > 200){
+        input_arr_y.shift()
+ 
+    }
     
-    plotlyMultiLinePlot(live_output_container, [{x: input_arr_x, y: input_arr_y}])
-}, false);
+    plotlyMultiLinePlot(live_input_container, [{x: input_arr_x, y: input_arr_y}])
+    const {zeros, poles} = filter_plane.getZerosPoles(radius)
+    console.log({zeros, poles})
+  
+    const [a, b] = await get_differenceEquationCoefficients(zeros, poles)
+    
+    var ya = output_arr_y.slice(-a.length+1);
+    ya = ya.reverse()
+    var xb = input_arr_y.slice(-b.length+1);
+    xb = xb.reverse()
+    console.log({a,b})
+    //summision
+    var new_value = 0
+    for (let i=0; i<ya.length; i++){
+        if (i == 0){
+            new_value += (xb[i]*b[i])
+        }
+        else{
+            new_value += (ya[i])*(-a[i])+(xb[i]*b[i])
+        }
+   
+    
+    }
+    output_arr_y.push(new_value)
+    output_arr_y.shift()
+  
+
+    plotlyMultiLinePlot(live_output_container, [{x: input_arr_x, y: output_arr_y}])
+});
 
 //Get Mouse Position
 function getMousePos(canvas, evt) {
@@ -55,6 +79,14 @@ function getMousePos(canvas, evt) {
     };
 }
 /*end b3bsa*/
+function equateLength(a, b){
+    max_length = Math.max(a.length, b.length)
+    for(let i = 0; i < max_length; i++){
+        a[i] = i < a.length ? a[i] : 0
+        b[i] = i < b.length ? b[i] : 0
+    }
+    return [a, b]
+}
 
 async function get_differenceEquationCoefficients(zeros, poles) {
     const {a, b} = await postData(`${API}/differenceEquationCoefficients`, {
